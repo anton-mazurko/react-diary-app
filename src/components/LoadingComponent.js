5d8bc62d1bbe9b864175696441ef0ea59e7db937
Anton Mazurko <anton.mazurko@gmail.com>
2021-10-17 18:15:19 +0200

    Styling loading component

--- src/components/LoadingComponent.js
+++ src/components/LoadingComponent.js
@@ -3,6 +3,7 @@ import {connect} from 'react-redux';
 import {withRouter} from 'react-router-dom';
 import {getUser} from '../actions/userAction';
 import {getNotes} from '../actions/notesAction';
+import Loading from '../components/Loading';
 
 class LoadingComponent extends Component {
   
@@ -29,9 +30,7 @@ class LoadingComponent extends Component {
     if ((userLoading === false && notesLoading === false) || this.props.user !== null) {
       return <div>{children}</div>;
     } else {
-      return(
-        <div><h2>Loading...</h2></div>
-      );
+      return <Loading />;
     }
   }
 }
