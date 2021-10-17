d17a020476d6cc1eeab88f589f47e8e679d2c8a0
Anton Mazurko <anton.mazurko@gmail.com>
2021-10-17 15:21:48 +0200

    Populate note content to update

--- src/components/NoteEdit.js
+++ src/components/NoteEdit.js
@@ -5,11 +5,78 @@ import {Link} from 'react-router-dom';
  
  
 class NoteEdit extends Component {
+  constructor(props) {
+    super(props);
+    // state
+    this.state = {
+      title: this.props.note.title,
+      body: this.props.note.body,
+    }
+    //bind
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-  render () {
+
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log('submit')
+    const note = {
+      title: this.state.title,
+      body: this.state.body,
+      uid: this.props.uid
+    }
+    // this.props.saveNote(note)
+    this.setState({
+      title: '',
+      body: ''
+    });
+  }
+
+
+  render() {
     return (
       <div className="container-fluid">
-        <h2>note edit page</h2>
+        <div className="row">
+          <div className="col-sm-6 col-sm-offset-3">
+            <form onSubmit={this.handleSubmit}>
+              <div className="form-group">
+                <input 
+                  onChange={this.handleChange}
+                  value={this.state.title}
+                  type="text"
+                  name="title"
+                  className="form-control no-border"
+                  placeholder="Title..."
+                  required
+                />
+              </div>
+
+              <div className="form-group">
+                <textarea 
+                  onChange={this.handleChange}
+                  value={this.state.body}
+                  type="text"
+                  name="body"
+                  className="form-control no-border"
+                  placeholder="Body..."
+                  required
+                />
+              </div>
+
+              <div className="form-group">
+                <button className="btn btn-primary col-sm-12">Update</button>
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
     );
   }
